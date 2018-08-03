3b85ef4094e4f0e878899ba6ca5c6e5953c5d644
Vinh Nguyen <vnguye34@students.kennesaw.edu>
2018-08-03 01:06:00 -0400

    wrote cnn with cuda

--- pytorch_MINST/main.py
+++ pytorch_MINST/main.py
@@ -1,112 +1,103 @@
-#  https://github.com/pytorch/examples/blob/master/mnist/main.py
+import torch #Import the pytorch enviroment to python
+import torchvision #load datasets
 
-from __future__ import print_function
+#import and load data
+#
+train_loader = torch.utils.data.DataLoader(
+torchvision.datasets.MNIST('./files/', train=True, download=True,
+						 transform=torchvision.transforms.Compose([
+						   torchvision.transforms.ToTensor(),
+						   torchvision.transforms.Normalize(
+							 (0.1307,), (0.3081,))
+						 ])),
+batch_size=64, shuffle=True)
 
-import argparse
-import torch
+test_loader = torch.utils.data.DataLoader(
+torchvision.datasets.MNIST('./files/', train=False, download=True,
+						 transform=torchvision.transforms.Compose([
+						   torchvision.transforms.ToTensor(),
+						   torchvision.transforms.Normalize(
+							 (0.1307,), (0.3081,))
+						 ])),
+batch_size=1000, shuffle=True)
+examples = enumerate(test_loader)
+batch_idx, (data, target) = next(examples)
+data.shape
+print("There are:",data.shape,";1000 examples of 28x28 pixels in greyscale")
+
+
+#neural network is here
 import torch.nn as nn
+from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
+use_cuda = torch.cuda.is_available()
 
+#define neural network
 class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+	def __init__(self):
+		super(Net, self).__init__()
+		self.conv1 = nn.Conv2d(1, 100, kernel_size=5)
+		self.conv2 = nn.Conv2d(100, 20, kernel_size=5)
+		self.conv2_drop = nn.Dropout2d()
+		self.fc1 = nn.Linear(320, 50)
+		self.fc2 = nn.Linear(50, 10)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+	def forward(self, x):
+		x = F.relu(F.max_pool2d(self.conv1(x), 2))
+		x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+		x = x.view(-1, 320)
+		x = F.relu(self.fc1(x))
+		x = F.dropout(x, training=self.training)
+		x = self.fc2(x)
+		return F.log_softmax(x)
+	
+n_epochs = 3
+learning_rate = 0.01
+momentum = 0.5
+random_seed = 1
+log_interval = 10
+				  
+model = Net()
+if use_cuda:
+	model = model.cuda()
+	
+optimizer = optim.SGD(model.parameters(), lr=learning_rate,
+				  momentum=momentum)
+					 
+def train(epoch):
+	model.train()
+	for batch_idx, (data, target) in enumerate(train_loader):
+		optimizer.zero_grad()
+		if use_cuda:
+			data, target = data.cuda(), target.cuda()
+		data, target = Variable(data), Variable(target)
+		output = model(data)
+		loss = F.nll_loss(output, target)
+		loss.backward()
+		optimizer.step()
+		if batch_idx % log_interval == 0:
+		  print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+			epoch, batch_idx * len(data), len(train_loader.dataset),
+			100. * batch_idx / len(train_loader), loss.item()))
+	
+def test():
+	model.eval()
+	test_loss = 0
+	correct = 0
+	for data, target in test_loader:
+		if use_cuda:
+			data, target = data.cuda(), target.cuda()
+		data, target = Variable(data, volatile=True), Variable(target)
+		output = model(data)
+		test_loss += F.nll_loss(output, target, size_average=False).item()
+		pred = output.data.max(1, keepdim=True)[1]
+		correct += pred.eq(target.data.view_as(pred)).sum()
+		test_loss /= len(test_loader.dataset)
+		print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+		test_loss, correct, len(test_loader.dataset),
+		100. * correct / len(test_loader.dataset)))
 
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-
-def test(args, model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
-            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
-def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                        help='SGD momentum (default: 0.5)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-
-    torch.manual_seed(args.seed)
-
-    device = torch.device("cuda" if use_cuda else "cpu")
-
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.test_batch_size, shuffle=True, **kwargs)
-
-
-    model = Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
-
-
-if __name__ == '__main__':
-	main()
+for epoch in range(1, n_epochs + 1):
+	train(epoch)
+	test()
