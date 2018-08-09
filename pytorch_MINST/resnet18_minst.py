70caa909313b082209447bd227246543331a7c25
Vinh Nguyen <vnguye34@students.kennesaw.edu>
2018-08-08 23:20:07 -0400

    rewrite entire thing

--- pytorch_MINST/resnet18_minst.py
+++ pytorch_MINST/resnet18_minst.py
@@ -1,119 +1,35 @@
-from __future__ import print_function, division
-
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-from torch.autograd import Variable
-import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import time
-import os
-import copy
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
 
-plt.ion()   # interactive mode
+# Load MINST dataset
+mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
 
-# Load MINST
+mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
 
-# Data to train neural network
-train_data = torch.utils.data.DataLoader(
-torchvision.datasets.MNIST('./files/', train=True, download=True,
-						 transform=torchvision.transforms.Compose([
-						   torchvision.transforms.ToTensor(),
-						   torchvision.transforms.Normalize(
-							 (0.1307,), (0.3081,))
-						 ])),
-batch_size=1000, shuffle=True)
+print("Testset: ",len(mnist_trainset))
+print("Trainset: ", len(mnist_testset))
 
-# Separate data to test against network
-test_data = torch.utils.data.DataLoader(
-torchvision.datasets.MNIST('./files/', train=False, download=True,
-						 transform=torchvision.transforms.Compose([
-						   torchvision.transforms.ToTensor(),
-						   torchvision.transforms.Normalize(
-							 (0.1307,), (0.3081,))
-						 ])),
-batch_size=100, shuffle=True)
-examples = enumerate(test_data)
-batch_idx, (data, target) = next(examples)
-data.shape
-
-print("There are:",data.shape,";1000 examples of 28x28 pixels in greyscale")
-
+#
 # Sanity check for CUDA or CPU 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# Train the model
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
 
-					 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-	model.train()
-	
-	for batch_idx, (data, target) in enumerate(train_data):
-		optimizer.zero_grad()
-		
-		if torch.device("cuda:0"):
-			data, target = data.cuda(), target.cuda()
-			
-		data, target = Variable(data), Variable(target)
-		output = model(data)
-		loss = F.nll_loss(output, target)
-		loss.backward()
-		optimizer.step()
-		
-		if batch_idx % log_interval == 0:
-		  print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-			epoch, batch_idx * len(data), len(train_loader.dataset),
-			100. * batch_idx / len(train_loader), loss.item()))
+#load in pretrained network
 
-# Display predictions at the end
-def visualize_model(model, num_images=6):
-    was_training = model.training
-    model.eval()
-    images_so_far = 0
-    fig = plt.figure()
+# =============================================================================
+# All pre-trained models expect input images normalized in the same way, i.e. 
+# mini-batches of 3-channel RGB images of shape (3 x H x W), where H and W are 
+# expected to be at least 224. The images have to be loaded in to a range of 
+# [0, 1] and then normalized using mean = [0.485, 0.456, 0.406] and 
+# std = [0.229, 0.224, 0.225]. You can use the following transform to 
+#normalize:
+#
+#normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+#                             std=[0.229, 0.224, 0.225])
+# =============================================================================
 
-    with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders['val']):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-
-            for j in range(inputs.size()[0]):
-                images_so_far += 1
-                ax = plt.subplot(num_images//2, 2, images_so_far)
-                ax.axis('off')
-                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
-                imshow(inputs.cpu().data[j])
-
-                if images_so_far == num_images:
-                    model.train(mode=was_training)
-                    return
-        model.train(mode=was_training)
-        
-# Load Pretrained model, Reset18, reset the final fully connected layer
-# aka "chop off"
-model_ft = models.resnet18(pretrained=True)
-num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Linear(num_ftrs, 2)
-
-model_ft = model_ft.to(device)
-
-criterion = nn.CrossEntropyLoss()
-
-# Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-
-# Train and evaluate the data
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
-                       
-# show sample between loaded images and predictions                       
-visualize_model(model_ft)
+resnet18 = models.resnet18(pretrained=True)
