d3d1503742de4401c88458a2ad13888a10316c8e
Vinh Nguyen <vnguye34@students.kennesaw.edu>
2018-08-10 01:19:40 -0400

    organizational move

---
+++ example_work/test2.py
@@ -0,0 +1,34 @@
+import torch #Import the pytorch enviroment to python
+import torchvision #load datasets
+
+#import and load data
+#
+train_loader = torch.utils.data.DataLoader(
+torchvision.datasets.MNIST('./files/', train=True, download=True,
+						 transform=torchvision.transforms.Compose([
+						   torchvision.transforms.ToTensor(),
+						   torchvision.transforms.Normalize(
+							 (0.1307,), (0.3081,))
+						 ])),
+batch_size=64, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+torchvision.datasets.MNIST('./files/', train=False, download=True,
+						 transform=torchvision.transforms.Compose([
+						   torchvision.transforms.ToTensor(),
+						   torchvision.transforms.Normalize(
+							 (0.1307,), (0.3081,))
+						 ])),
+batch_size=1000, shuffle=True)
+examples = enumerate(test_loader)
+batch_idx, (data, target) = next(examples)
+data.shape
+print("There are:",data.shape,";1000 examples of 28x28 pixels in greyscale")
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+for i in range(6):
+    ax = fig.add_subplot(231+i)
+    ax.imshow(data[i][0])
+plt.show()
+fig
